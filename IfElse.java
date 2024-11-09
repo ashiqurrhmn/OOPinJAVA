1462ec4086a812b37c14acc2c705e8f02559ff2e
Md. Ashiqur Rahman Akash <138354564+ashiqurrhmn@users.noreply.github.com>
2024-11-09 22:31:59 +0600

    Add files via upload

---
+++ IfElse.java
@@ -0,0 +1,12 @@
+public class IfElse {
+    public static void main(String[] args) {
+        int mark = 82;
+        if(mark > 80){
+            System.out.println("A+");
+        }else if(mark > 40){
+            System.out.println("Pass");
+        }else{
+            System.out.println("Fail");
+        }
+    }
+}
